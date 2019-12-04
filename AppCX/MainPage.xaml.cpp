2bed8e4db19871b358e33ced02bced964fdf6f85
Fay Wang <v-faywan@microsoft.com>
2019-12-04 11:01:20 +0800

    Add project files.

---
+++ AppCX/MainPage.xaml.cpp
@@ -0,0 +1,32 @@
+﻿//
+// MainPage.xaml.cpp
+// Implementation of the MainPage class.
+//
+
+#include "pch.h"
+#include "MainPage.xaml.h"
+
+
+using namespace AppCX;
+
+using namespace Platform;
+using namespace Windows::Foundation;
+using namespace Windows::Foundation::Collections;
+using namespace Windows::UI::Xaml;
+using namespace Windows::UI::Xaml::Controls;
+using namespace Windows::UI::Xaml::Controls::Primitives;
+using namespace Windows::UI::Xaml::Data;
+using namespace Windows::UI::Xaml::Input;
+using namespace Windows::UI::Xaml::Media;
+using namespace Windows::UI::Xaml::Navigation;
+using namespace RuntimeComponent1;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+MainPage::MainPage()
+{
+	InitializeComponent();
+
+	
+
+}
---
+++ RuntimeComponent1/Class1.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AppCenter;
+using Microsoft.AppCenter.Analytics;
+using Microsoft.AppCenter.Crashes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RuntimeComponent1
+{
+    public sealed class Class1
+    {
+        public void InitAppCenter()
+        {
+            AppCenter.Start("afc4f213-f02e-4f74-ae42-6386100bdaf8", typeof(Analytics), typeof(Crashes));
+
+        }
+    }
+}
